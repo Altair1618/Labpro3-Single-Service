4509558fde2f5e8e756a989ab0411702149cb3fe
farhanns <farhannabilsuryono3@gmail.com>
2023-07-06 14:52:21 +0700

    feat: additional setup

---
+++ src/types/data.ts
@@ -0,0 +1,16 @@
+export type CompanyData = {
+    id: number;
+    name: string;
+    address: string;
+    phone: string;
+    taxCode: string;
+}
+
+export type ItemData = {
+    id: number;
+    name: string;
+    price: number;
+    stock: number;
+    code: string;
+    producerId: number;
+}
