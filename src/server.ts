a04fd395ba9d1a39f3599de06fd825239d069936
farhanns <farhannabilsuryono3@gmail.com>
2023-07-06 09:06:29 +0700

    feat: initial setup

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import express, {Express, Request, Response} from 'express';
+
+const app: Express = express();
+const port = 3000;
+
+app.listen(port, ()=> {
+    console.log(`Server started at https://localhost:${port}`);
+});
